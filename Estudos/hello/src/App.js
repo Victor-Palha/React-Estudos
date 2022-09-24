ca868b6600cb51cab65a7bbee13f2e53bccecbd1
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-09-24 19:29:44 -0300

    update css and component

--- Estudos/hello/src/App.js
+++ Estudos/hello/src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="main">
+        <h1>Hello From React</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis illum veritatis, at distinctio vel optio fugit magnam aliquid. Et quisquam dolorem neque exercitationem praesentium est illum vitae nisi iusto amet.</p>
+      </div>
     </div>
   );
 }
