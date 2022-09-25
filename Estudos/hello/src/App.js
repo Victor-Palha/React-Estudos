5287ce25f83dfeff1ae3e99f1455ce5d77c13d9b
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-09-25 19:52:14 -0300

    Create first component

--- Estudos/hello/src/App.js
+++ Estudos/hello/src/App.js
@@ -1,14 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
-      <div className="main">
-        <h1>Hello From React</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis illum veritatis, at distinctio vel optio fugit magnam aliquid. Et quisquam dolorem neque exercitationem praesentium est illum vitae nisi iusto amet.</p>
-      </div>
+      <h1>Fundamentos React</h1>
     </div>
   );
 }
---
+++ Estudos/hello/src/components/FirstComponent.js
@@ -0,0 +1,9 @@
+const FirstComponent = ()=>{
+    return(
+        <>
+            <h2>Primeiro Componente :)</h2>
+        </>
+    )
+}
+
+export default FirstComponent
