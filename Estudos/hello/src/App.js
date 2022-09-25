2bd20f54891c3c5e7b61e42c04b1f111f36e98c7
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-09-25 19:57:00 -0300

    Import component

--- Estudos/hello/src/App.js
+++ Estudos/hello/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import FirstComponent from './components/FirstComponent';
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
+      <FirstComponent/>
     </div>
   );
 }
