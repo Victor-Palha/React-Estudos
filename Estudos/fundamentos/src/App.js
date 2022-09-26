e85d2747ea552c868412ca487dba367cf8016515
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-09-26 11:59:18 -0300

    import img from assets

--- Estudos/fundamentos/src/App.js
+++ Estudos/fundamentos/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Astral from "./assets/Astral.png"
 
 function App() {
   return (
     <div className="App">
       <h1>React section 3</h1>
+      {/* Img from Public Folder */}
       <div>
         <img src="/dnd.png" alt="dnd img"/>
+      </div>
+      {/* Img from Assets */}
+      <div className="astral">
+        <img src={Astral} alt="alternative cover Astral adventure Guide"/>
       </div>
     </div>
   );
