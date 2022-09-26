32c1fa62cccd19722cc4b70475ed8a4c3da67bd1
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-09-26 11:49:27 -0300

    img in public folder

--- Estudos/fundamentos/src/App.js
+++ Estudos/fundamentos/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>React section 3</h1>
+      <div>
+        <img src="/dnd.png" alt="dnd img"/>
+      </div>
     </div>
   );
 }
