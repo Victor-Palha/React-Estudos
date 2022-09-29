8af091ffb8f36cb4eaa629e8b7124e47c61540cf
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-09-29 11:06:21 -0300

    Dinamic css inline

--- Estudos/react-css/src/App.js
+++ Estudos/react-css/src/App.js
@@ -1,14 +1,26 @@
 
 import './App.css';
 import MyComponent from './components/MyComponent';
+import {useState} from "react"
 
 function App() {
+  const [vali, setVali] = useState(10)
+  function handleText(){
+    if(vali === 10){
+      setVali(20)
+    }else{
+      setVali(10)
+    }
+  }
   return (
     <div className="App">
       {/* Global css */}
       <h1>Hello from React</h1>
       {/* Css component */}
       <MyComponent/>
+      {/* Dinamic css inline */}
+      <h2 style={ vali < 15 ? ({color:"yellow", backgroundColor:"black", margin:"10px"}) : ({color:"blue", backgroundColor:"grey", margin:"10px"}) }>Condicional css inline</h2>
+      <button onClick={handleText}>Change Text</button>
     </div>
   );
 }
