2e7333afb8c1131be2a88574fd57ac11e3d68ed7
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-09-29 11:15:58 -0300

    Dinamic class

--- Estudos/react-css/src/App.js
+++ Estudos/react-css/src/App.js
@@ -5,6 +5,7 @@ import {useState} from "react"
 
 function App() {
   const [vali, setVali] = useState(10)
+  let dinamic = false
   function handleText(){
     if(vali === 10){
       setVali(20)
@@ -21,6 +22,8 @@ function App() {
       {/* Dinamic css inline */}
       <h2 style={ vali < 15 ? ({color:"yellow", backgroundColor:"black", margin:"10px"}) : ({color:"blue", backgroundColor:"grey", margin:"10px"}) }>Condicional css inline</h2>
       <button onClick={handleText}>Change Text</button>
+      {/* Dinamic class */}
+      <h3 className={dinamic ? "true-title" : "false-title"}>Dinamic class</h3>
     </div>
   );
 }
