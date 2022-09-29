79cb935deab253a758087229ba908a66e8c4a8a9
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-09-29 10:52:54 -0300

    Component css

--- Estudos/react-css/src/App.js
+++ Estudos/react-css/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
       {/* Global css */}
       <h1>Hello from React</h1>
+      {/* Css component */}
+      <MyComponent/>
     </div>
   );
 }
---
+++ Estudos/react-css/src/components/MyComponent.js
@@ -0,0 +1,10 @@
+import "./MyComponent.scss"
+const MyComponent = ()=>{
+    return(
+        <>
+            <h1>My Component</h1>
+            <p className="paragraph-comp">Hello World</p>
+        </>
+    )
+}
+export default MyComponent
