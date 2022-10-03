718630e35a33e23a8a7a103213f581016b9fb028
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-10-03 11:32:42 -0300

    data management

--- Estudos/forms/src/component/MyForm.js
+++ Estudos/forms/src/component/MyForm.js
@@ -1,26 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./MyForm.css"
 
 const MyForm = () => {
-  return (
-    <div>
-        {/*Criação do form */}
-        <form>
-            <div>
-                <label htmlFor='nome'>Nome:</label>
-                <input type="text" name='nome' placeholder='Digite seu nome'/>
-            </div>
-            {/*Label envolvendo INPUT */}
-            <div>
-                <label>
-                    <span>E-mail: </span>
-                    <input type="email" placeholder='Digite seu E-mail' name="E-mail"/>
-                </label>
-            </div>
-                <input type="submit"/>
-        </form>
-    </div>
-  )
+    // Gerenciamento de dados
+    const [nome, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const handleName = (e)=>{
+        setName(e.target.value)
+    }
+    console.log(nome)
+    return (
+        <div>
+            {/*Criação do form */}
+            <form>
+                <div>
+                    <label htmlFor='nome'>Nome:</label>
+                    <input type="text" name='nome' placeholder='Digite seu nome' onChange={handleName}/>
+                </div>
+                {/*Label envolvendo INPUT */}
+                <div>
+                    <label>
+                        <span>E-mail: </span>
+                        <input type="email" placeholder='Digite seu E-mail' name="E-mail"/>
+                    </label>
+                </div>
+                    <input type="submit"/>
+            </form>
+        </div>
+    )
 }
 
 export default MyForm
