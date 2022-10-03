2e1ea0bb553fc814628212aed7d899bca07d98d3
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-10-03 12:21:15 -0300

    textarea and select

--- Estudos/forms/src/component/MyForm.js
+++ Estudos/forms/src/component/MyForm.js
@@ -5,6 +5,8 @@ const MyForm = ({user}) => {
     // Gerenciamento de dados
     const [nome, setName] = useState(user? user.name : "")
     const [email, setEmail] = useState(user ? user.email : "")
+    const [bio, setBio] = useState("")
+    const [role, setRole] = useState("")
     const handleName = (e)=>{
         setName(e.target.value)
     }
@@ -14,8 +16,12 @@ const MyForm = ({user}) => {
         e.preventDefault()
         console.log(nome)
         console.log(email)
+        console.log(bio)
+        console.log(role)
         setName("")
         setEmail("")
+        setBio("")
+        setRole("")
     }
     return (
         <div>
@@ -30,6 +36,22 @@ const MyForm = ({user}) => {
                     <label>
                         <span>E-mail: </span>
                         <input type="email" placeholder='Digite seu E-mail' name="E-mail" onChange={(e)=>setEmail(e.target.value)} value={email}/>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <span>Bio:</span>
+                        <textarea placeholder='Sua Descrição' name="bio" onChange={(e) => setBio(e.target.value)} value={bio}/>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <span>Função</span>
+                        <select onChange={(e)=>setRole(e.target.value)}>
+                            <option value="progamador jr">Programador Jr</option>
+                            <option value="programador pleno">Programador Pleno</option>
+                            <option value="programdor senior">Programador Senior</option>
+                        </select>
                     </label>
                 </div>
                     <input type="submit"/>
