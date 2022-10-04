85d8a8cd3041e9ada4847d2317bd8d797228b0fa
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-10-04 12:16:58 -0300

    Fetch api datas

--- Estudos/req-http/src/App.js
+++ Estudos/req-http/src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import {useState, useEffect} from "react"
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  const url = "http://localhost:3000/products"
+
+  //Resgatando dados
+  useEffect(()=>{
+    async function getData(url){
+
+      const res = await fetch(url)
+      const data = await res.json()
+
+      setProducts(data)
+    }
+
+    getData(url)
+
+  },[])
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product)=>(
+          <li key={product.id}>{product.name} - R$: {product.price}</li>
+        ))}
+      </ul>
     </div>
   );
 }
