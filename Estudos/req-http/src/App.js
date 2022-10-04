4863b9ac6e58d44874f14099445510f646e1360f
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-10-04 12:57:23 -0300

    POST HTTP AND VIEW

--- Estudos/req-http/src/App.js
+++ Estudos/req-http/src/App.js
@@ -3,6 +3,8 @@ import {useState, useEffect} from "react"
 
 function App() {
   const [products, setProducts] = useState([])
+  const [name, setName] = useState()
+  const [price, setPrice] = useState()
 
   const url = "http://localhost:3000/products"
 
@@ -19,6 +21,25 @@ function App() {
     getData(url)
 
   },[])
+  //Adicionando datas
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+    const product = {
+      name,
+      price
+    }
+    const res = await fetch(url,{
+      method: "POST",
+      headers: {
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify(product)
+    })
+
+    //Carregamento dinamico
+    const addedProduct = await res.json()
+    setProducts((prevProducts) => [...prevProducts, addedProduct])
+  }
 
   return (
     <div className="App">
@@ -28,6 +49,19 @@ function App() {
           <li key={product.id}>{product.name} - R$: {product.price}</li>
         ))}
       </ul>
+      <div className='add-product'>
+          <form onSubmit={handleSubmit}>
+            <label>
+              <span>Nome:</span>
+              <input type="text" value={name} onChange={(e)=> setName(e.target.value)}/>
+            </label>
+            <label>
+              <span>Preço:</span>
+              <input type="number" value={price} onChange={(e)=> setPrice(e.target.value)}/>
+            </label>
+            <input type="submit" value="Criar"/>
+          </form>
+      </div>
     </div>
   );
 }
