cabee057c55b9e91a9d0ef53f1593d04fc9e2405
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-10-16 19:26:57 -0300

    loading

--- Estudos/req-http/src/hooks/useFetch.js
+++ Estudos/req-http/src/hooks/useFetch.js
@@ -9,6 +9,9 @@ export const useFetch = (url)=>{
     const [method, setMethod] = useState(null)
     const [callFetch, setCallFetch] = useState(false)
 
+    //Loading
+    const [loading, setLoading] = useState(false)
+
     const httpConfig = (data, method)=>{
         if(method === "POST"){
             setConfig({
@@ -25,10 +28,12 @@ export const useFetch = (url)=>{
 
     useEffect(()=>{
         const fetchData = async ()=>{
+            setLoading(true)
             const res = await fetch(url)
             const json = await res.json()
 
             setData(json)
+            setLoading(false)
         }
 
         fetchData()
@@ -49,5 +54,5 @@ export const useFetch = (url)=>{
        httpRequest()
     },[config, method, url])
 
-    return {data, httpConfig}
+    return {data, httpConfig, loading}
 }
