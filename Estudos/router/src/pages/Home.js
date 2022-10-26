75e37fc9117d9fe5b0bd7f1b9ee3dcab2f773977
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-10-25 22:15:27 -0300

    loading data

--- Estudos/router/src/pages/Home.js
+++ Estudos/router/src/pages/Home.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import "./Home.css"
+import { Link } from 'react-router-dom'
+import {useFetch} from '../hooks/useFetch'
 
 const Home = () => {
+    const url = "http://localhost:3000/products"
+    const {data:items, loading, error} = useFetch(url)
   return (
     <div>
-        <p>Home</p>
+        <h1>PRODUTOS</h1>
+        {error && <p>{error}</p>}
+        <ul className='products'>
+            {items && items.map(item => (
+                <li key={item.id}>
+                    <h2>{item.name}</h2>
+                    <p>R$ {item.price}</p>
+                </li>
+            ))}
+        </ul>
     </div>
   )
 }
