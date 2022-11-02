c78253868e7500bd9c011567561d4752252ec83e
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-11-01 21:53:30 -0300

    create pages and navbar
    using React Router Dom to create pages and components to navigate in the application

--- Estudos/context/src/App.js
+++ Estudos/context/src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import './App.scss';
+//components
+import NavBar from './components/NavBar';
+//router
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+//Pages
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Products from './pages/Products';
+import About from './pages/About';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from react - Context</h1>
+      <BrowserRouter>
+        <NavBar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/products' element={<Products/>}/>
+          <Route path='/about' element={<About/>}/>
+          
+          {/*404 page*/}
+          <Route path='*' element={<NotFound/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
