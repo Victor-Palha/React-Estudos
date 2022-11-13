7f89bc6d5f14cfef616a6949dc064959a1846f80
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-11-13 13:47:08 -0300

    Estilos de CSS

--- Projeto Blog/dmblog/src/components/Footer.js
+++ Projeto Blog/dmblog/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from "./Footer.module.css"
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <h3>Um Blog para DMs!</h3>
       <p>DM Blog &copy; 2022</p>
     </footer>
