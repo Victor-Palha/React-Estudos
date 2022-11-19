0f16d3c5c65e8a773da30c5bae7a68ad2de5d45e
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-11-19 17:51:07 -0300

    Links para usuário autenticado

--- Projeto Blog/dmblog/src/App.js
+++ Projeto Blog/dmblog/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom"
+import { onAuthStateChanged } from 'firebase/auth';
+//Hooks
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
 //Context
-import { AuthProvider } from './pages/context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 //Pages
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
@@ -10,11 +14,30 @@ import Footer from './components/Footer';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import NotFound from './pages/NotFound/NotFound';
+import CreatePost from './pages/CreatePost/CreatePost';
+import Dashboard from './pages/Dashboard/Dashboard';
+
 
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const {auth} = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user)=>{
+      setUser(user)
+    })
+  }, [auth])
+
+  if(loadingUser){
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
-      <AuthProvider>
+      <AuthProvider value={{user}}>
         <BrowserRouter>
           <Navbar/>
           <div className='container'>
@@ -23,6 +46,8 @@ function App() {
               <Route path='/about' element={<About/>}/>
               <Route path='/login' element={<Login/>}/>
               <Route path='/register' element={<Register/>}/>
+              <Route path="/posts/create" element={<CreatePost/>} />
+              <Route path="/dashboard" element={<Dashboard/>} />
               {/* 404 */}
               <Route path='*' element={<NotFound/>}/>
             </Routes>
--- Projeto Blog/dmblog/src/components/Navbar.js
+++ Projeto Blog/dmblog/src/components/Navbar.js
@@ -1,7 +1,13 @@
 import React from 'react'
-import styles from "./Navbar.module.css"
 import {NavLink} from "react-router-dom"
+
+import {useAuthentication} from "../hooks/useAuthentication"
+
+import {useAuthValue} from "../context/AuthContext"
+
+import styles from "./Navbar.module.css"
 const Navbar = () => {
+  const {user} = useAuthValue()
   return (
     <nav className={styles.navbar}>
         <NavLink to="/" className={styles.brand}>
@@ -9,8 +15,18 @@ const Navbar = () => {
         </NavLink>
         <ul className={styles.links_list}>
             <li><NavLink to="/" className={({isActive}) => (isActive ? styles.active : "")}>Home</NavLink></li>
-            <li><NavLink to="/login" className={({isActive}) => (isActive ? styles.active : "")}>Login</NavLink></li>
-            <li><NavLink to="/register" className={({isActive}) => (isActive ? styles.active : "")}>Registrar</NavLink></li>
+            {!user && (
+              <>
+                <li><NavLink to="/login" className={({isActive}) => (isActive ? styles.active : "")}>Login</NavLink></li>
+                <li><NavLink to="/register" className={({isActive}) => (isActive ? styles.active : "")}>Registrar</NavLink></li>
+              </>
+            )}
+            {user && (
+              <>
+                <li><NavLink to="/posts/create" className={({isActive}) => (isActive ? styles.active : "")}>Novo Posts</NavLink></li>
+                <li><NavLink to="/dashboard" className={({isActive}) => (isActive ? styles.active : "")}>Dashboard</NavLink></li>
+              </>
+            )}
             <li><NavLink to="/about" className={({isActive}) => (isActive ? styles.active : "")}>Sobre</NavLink></li>
         </ul>
     </nav>
