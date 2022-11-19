fdda73bbb111a2d57ddc7989d04d83a78427ce89
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-11-19 18:15:59 -0300

    Login de usuário

--- Projeto Blog/dmblog/src/hooks/useAuthentication.js
+++ Projeto Blog/dmblog/src/hooks/useAuthentication.js
@@ -47,7 +47,7 @@ export const useAuthentication = ()=>{
             console.log(typeof error.message)
 
             let systemErrorMessage
-            if(error.message.includes("PASSWORD")){
+            if(error.message.includes("Password")){
                 systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres."
             }else if(error.message.includes("email-already")){
                 systemErrorMessage = "E-mail já cadastrado"
@@ -64,6 +64,28 @@ export const useAuthentication = ()=>{
         checkIfIsCancelled()
         signOut(auth)
     }
+    //Login
+    const login = async(data)=>{
+        checkIfIsCancelled()
+
+        setLoading(true)
+        setError(false)
+        try {
+            await signInWithEmailAndPassword(auth, data.email, data.password)
+            setLoading(false)
+        } catch (error) {
+            let systemErrorMessage
+            if(error.message.includes("user-not-found")){
+                systemErrorMessage = "Usuário não encontrado."
+            }else if(error.message.includes("wrong-password")){
+                systemErrorMessage = "Senha incorreta."
+            }else{
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde."
+            }
+            setError(systemErrorMessage)
+            setLoading(false)
+        }
+    }
 
     useEffect(()=>{
         return () => setCancelled(true)
@@ -74,6 +96,7 @@ export const useAuthentication = ()=>{
         createUser,
         error,
         loading,
-        logout
+        logout,
+        login
     }
 }
--- Projeto Blog/dmblog/src/pages/Login/Login.js
+++ Projeto Blog/dmblog/src/pages/Login/Login.js
@@ -7,7 +7,7 @@ const Login = () => {
   const [password, setPassword] = useState("")
   const [error, setError] = useState("")
 
-  const {createUser, error: authError, loading} = useAuthentication()
+  const {error: authError, loading, login} = useAuthentication()
 
   const handleSubmit = async (e)=>{
     e.preventDefault()
@@ -18,7 +18,7 @@ const Login = () => {
       password
     }
 
-    const res = await createUser(user)
+    const res = await login(user)
     console.log(res)
   }
   useEffect(()=>{
