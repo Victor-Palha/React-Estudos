e2e775aafb2f195b6b0268c8bb8ac36496e2e75f
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-11-19 17:59:34 -0300

    Bloqueando páginas que precisam de autenticação

--- Projeto Blog/dmblog/src/pages/About/About.js
+++ Projeto Blog/dmblog/src/pages/About/About.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 //CSS
 import styles from "./About.module.css"
 
 const About = () => {
   return (
-    <div>
-        <h1>Sobre</h1>
+    <div className={styles.about}>
+        <h1>Sobre o DM <span>Blog</span></h1>
+        <p>Esse projeto consiste em um blog feito com React no front-end e Firebase no back-end</p>
+        <Link to="/posts/create" className='btn'>Criar Post</Link>
     </div>
   )
 }
